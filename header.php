25a7179e58312ccddc072bae5b746f83c8d7ef3f
pire <rob.mccormack@largeblue.com>
2011-10-07 11:16:16 +0200

    first round of design amends for header and homepage

--- header.php
+++ header.php
@@ -19,75 +19,69 @@
 <?php woo_top(); ?>
 
 <div id="wrapper">
-	
-	<?php if ( function_exists( 'has_nav_menu') && has_nav_menu( 'top-menu') ) { ?>
-	
-	<div id="top">
-		<div class="col-full">
-			<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'top-nav', 'menu_class' => 'nav fl', 'theme_location' => 'top-menu' ) ); ?>
-    		<div id="social-links">
-                <a href="http://youtube.com/greentvgreentv" target="_blank" title="green.tv on youtube"><img src="<?php bloginfo('template_directory'); ?>/images/social/yt.png" alt="Youtube" /></a>
-                <a href="http://twitter.com/green_tv" target="_blank" title="green.tv on twitter"><img src="<?php bloginfo('template_directory'); ?>/images/social/tw.png" alt="Twitter" /></a>
-                <a href="https://www.facebook.com/greentv" target="_blank" title="green.tv on facebook"><img src="<?php bloginfo('template_directory'); ?>/images/social/fb.png" alt="FaceBook" /></a>
-    		</div>
-		</div>
-	</div><!-- /#top -->
-	
-    <?php } ?>
 
 	<div id="header">
            
 		<div id="header-top">
 			<div class="col-full">
-	 		       
+
+            	<?php if ( function_exists( 'has_nav_menu') && has_nav_menu( 'top-menu') ) : ?>
+            	<div id="top">
+                    <?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'top-nav', 'menu_class' => 'nav fl', 'theme_location' => 'top-menu' ) ); ?>
+            	</div><!-- /#top -->
+                <?php endif; ?>
+
 				<div id="logo">
-			       
 				<?php if ($woo_options['woo_texttitle'] <> "true") : $logo = $woo_options['woo_logo']; ?>
 					<a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('description'); ?>">
 						<img src="<?php if ($logo) echo $logo; else { bloginfo('template_directory'); ?>/images/logo.png<?php } ?>" alt="<?php bloginfo('name'); ?>" />
 					</a>
 		        <?php endif; ?> 
-		        
+
 		        <?php if( is_singular() && !is_front_page() ) : ?>
 					<span class="site-title"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></span>
 		        <?php else : ?>
 					<h1 class="site-title"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
 		        <?php endif; ?>
-			      	
 				</div><!-- /#logo -->
-				
-				<div id="tagline" class="col-full">
-					<span class="site-description"><?php if (function_exists('iinclude_page')) iinclude_page(163) //bloginfo('description'); ?></span>
-				</div>
-				
-				<div id="search-top">
-					<?php get_template_part( 'search-form' ); ?>
-				</div>
 
+                <div id="main-navigation">
+    				<div id="search-top">
+    					<?php get_template_part( 'search-form' ); ?>
+    				</div>
 				<?php
 				if ( function_exists('has_nav_menu') && has_nav_menu('primary-menu') ) {
-					wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fr', 'theme_location' => 'primary-menu' ) );
+					wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fl', 'theme_location' => 'primary-menu' ) );
 				} else {
 				?>
-		        <ul id="main-nav" class="nav fl">
-					<?php 
-		        	if ( isset($woo_options['woo_custom_nav_menu']) AND $woo_options['woo_custom_nav_menu'] == 'true' ) {
-		        		if ( function_exists('woo_custom_navigation_output') )
-							woo_custom_navigation_output();
-					} else { ?>
-			            <?php if ( is_page() ) $highlight = "page_item"; else $highlight = "page_item current_page_item"; ?>
-			            <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>"><?php _e('Home', 'woothemes') ?></a></li>
-			            <?php 
-			    			wp_list_pages('sort_column=menu_order&depth=6&title_li=&exclude='); 
-					}
-					?>
-		        </ul><!-- /#nav -->
-		        <?php } ?> 	       
+    		        <ul class="nav">
+    					<?php 
+    		        	if ( isset($woo_options['woo_custom_nav_menu']) AND $woo_options['woo_custom_nav_menu'] == 'true' ) {
+    		        		if ( function_exists('woo_custom_navigation_output') )
+    							woo_custom_navigation_output();
+    					} else { ?>
+    			            <?php if ( is_page() ) $highlight = "page_item"; else $highlight = "page_item current_page_item"; ?>
+    			            <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>"><?php _e('Home', 'woothemes') ?></a></li>
+    			            <?php 
+    			    			wp_list_pages('sort_column=menu_order&depth=6&title_li=&exclude='); 
+    					}
+    					?>
+    		        </ul><!-- /#nav -->
+		        <?php } ?>
+		        </div><!-- /#main-nav -->  
 			</div>
 		</div><!-- /#header -->  
 		
 	</div><!-- /#header -->
 
-	<div id="partner-logos">
-		<?php if (function_exists('iinclude_page')) iinclude_page(246); ?>
+	<div class="col-full">
+		<?php /*if (function_exists('iinclude_page')) iinclude_page(246); */?>
+        <div id="social-links">
+            <a href="http://youtube.com/greentvgreentv" target="_blank" title="green.tv on youtube"><img src="<?php bloginfo('template_directory'); ?>/images/social/yt.png" alt="Youtube" /></a>
+            <a href="http://twitter.com/green_tv" target="_blank" title="green.tv on twitter"><img src="<?php bloginfo('template_directory'); ?>/images/social/tw.png" alt="Twitter" /></a>
+            <a href="https://www.facebook.com/greentv" target="_blank" title="green.tv on facebook"><img src="<?php bloginfo('template_directory'); ?>/images/social/fb.png" alt="FaceBook" /></a>
+        </div>
+		<div id="tagline">
+			<span class="site-description"><?php bloginfo('description'); ?></span>
+		</div>
 	</div>
