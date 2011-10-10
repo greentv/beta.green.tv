38b6b574e69cdf17b8aedfb1a0089c6ef78c15d8
Ian Mays <ian.mays@largeblue.com>
2011-10-10 17:07:32 +0100

    add ie6 update script

--- header.php
+++ header.php
@@ -13,6 +13,16 @@
 <?php wp_head(); ?>
 <?php woo_head(); ?>
 
+<!--[if IE 6]>
+<script type="text/javascript"> 
+	/*Load jQuery if not already loaded*/ if(typeof jQuery == 'undefined'){ document.write("<script type=\"text/javascript\"   src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\"></"+"script>"); var __noconflict = true; } 
+	var IE6UPDATE_OPTIONS = {
+		icons_path: "<?php echo bloginfo('template_directory'); ?>/ie6update/images/"
+	}
+</script>
+<script type="text/javascript" src="<?php echo bloginfo('template_directory'); ?>/ie6update/ie6update.js"></script>
+<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
